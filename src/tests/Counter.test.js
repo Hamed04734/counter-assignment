6f3f19b00a473fb9a08d31a2b192cf96c6b00743
Hamed04734 <hamed0473@gmail.com>
2024-11-20 21:22:05 -0500

    test_cases

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,39 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, fireEvent, screen } from "@testing-library/react";
+import Counter from "../components/Counter";
 
 beforeEach(() => {
-  // Render the Counter component here
-})
+  // render counter
+  render(<Counter />);
+});
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  //  getByRole to locate the heading 
+  const counterMessage = screen.getByRole('heading', { name: /counter/i });
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  //  getByText to locate  count element directly
+  const initialCountElement = screen.getByText("0");
+  expect(initialCountElement).toBeInTheDocument();
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  // getByRole to locate the increment button
+  const incrementButton = screen.getByRole('button', { name: '+' });
+  fireEvent.click(incrementButton);
+
+  //  getByTestId to locate the count element by its test ID
+  const updatedCount = screen.getByTestId("count");
+  expect(updatedCount).toHaveTextContent("1");
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  // getByText to locate the decrement button
+  const decrementButton = screen.getByText("-");
+  fireEvent.click(decrementButton);
+
+  // Using getByTestId to locate the count element by its test ID
+  const updatedCount = screen.getByTestId("count");
+  expect(updatedCount).toHaveTextContent("-1");
 });
